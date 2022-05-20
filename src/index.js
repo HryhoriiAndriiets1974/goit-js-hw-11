f7719c62d087258566fc51484c005fe3802142f9
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-05-20 13:22:04 +0300

    +refs

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import './sass/main.scss';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import getRefs from './js/refs';
+
+const refs = getRefs();
+
+refs.searchForm.addEventListener('submit', onSearch);
+
+function onSearch(e) {
+  e.preventDefault();
+}
---
+++ src/js/refs.js
@@ -0,0 +1,9 @@
+export default function getRefs() {
+  return {
+    searchForm: document.querySelector('.search-form'),
+    searchInput: document.querySelector('.search-input'),
+    searchBtn: document.querySelector('.search-btn'),
+    imgGallery: document.querySelector('.gallery'),
+    loadMore: document.querySelector('.load-more'),
+  }
+}
