50ed8a102dbd86f3184ebe987590fbe727ee0f8c
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-05-20 14:09:04 +0300

    +img-service

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import './sass/main.scss';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import getRefs from './js/refs';
+import ImgApiService from './js/img-service';
 
 const refs = getRefs();
+const imgApiService = new ImgApiService();
 
 refs.searchForm.addEventListener('submit', onSearch);
 
