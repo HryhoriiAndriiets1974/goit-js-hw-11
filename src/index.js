008030a8aa3634a684afd2d282ef4118ed3282fd
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-05-20 15:34:18 +0300

    oppa

--- src/index.js
+++ src/index.js
@@ -11,4 +11,21 @@ refs.searchForm.addEventListener('submit', onSearch);
 
 function onSearch(e) {
   e.preventDefault();
+  clearImgGallary();
+  imgApiService.query = e.currentTarget.elements.searchQuery.value;
+  imgApiService.resetPage();
+
+  imgApiService.fetchImg()
+  .then(({hits, totalHits}) => {
+    Notify.info(`Hooray! We found ${totalHits} images.`);
+    appendCardMarkup(hits);
+  });
+}
+
+function appendCardMarkup (data) {
+  refs.imgGallery.insertAdjacentHTML('beforeend', imgCard(data));
+}
+
+function clearImgGallary() {
+  refs.imgGallery.innerHTML = "";
 }
