b1a9be60c11fd09f2cdb599c98b46ba5b895ea4d
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-05-20 15:10:35 +0300

    +img-card

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './sass/main.scss';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import getRefs from './js/refs';
 import ImgApiService from './js/img-service';
+import imgCard from'./templates/img-card.hbs';
 
 const refs = getRefs();
 const imgApiService = new ImgApiService();
